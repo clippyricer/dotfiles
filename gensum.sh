f72f70cc79efe24f860e3f578926c48d95b1c938
clippyricer <bannansmooth68@proton.me>
2026-03-03 01:49:21 +0000

    fix gensum

--- gensum.sh
+++ gensum.sh
@@ -3,23 +3,21 @@ cache="$HOME/.cache/gensum.cache"
 mkdir -p "$HOME/.cache/"
 
 ARCH_HASH=$(md5sum dependencies/basic-arch.txt | cut -d' ' -f1)
-echo $ARCH_HASH
+echo "NEW HASH IS $ARCH_HASH"
 
 if [ -f "$HOME/.cache/gensum.cache" ]; then
     ARCH_LOADED_HASH=$(cat "$HOME/.cache/gensum.cache")
-    echo $ARCH_LOADED_HASH
+    echo "CURRENT HASH IS $ARCH_LOADED_HASH"
+    sleep 2
 else
     ARCH_LOADED_HASH="none"
-    echo $ARCH_LOADED_HASH
+    echo "CURRENT HASH IS $ARCH_LOADED_HASH"
+    sleep 2
     echo "Creating hash..."
     touch $HOME/.cache/gensum.cache
     echo ""
-    if [ ! -s "$cache" ]; then
-        echo "$ARCH_HASH" > "$cache"
-    else
-        sed -i "1s/.*/$ARCH_HASH/" "$cache"
-    fi
 fi
+
 
 if [[ "$ARCH_HASH" == "$ARCH_LOADED_HASH" ]]; then
     echo "Dependencies haven't changed. Keeping cache."
@@ -27,4 +25,9 @@ else
     echo "Dependencies changed or cache missing. Reseting cache..."
     curl -LO https://raw.githubusercontent.com/clippyricer/dotfiles/refs/heads/main/setup.cache
     cp setup.cache "$HOME/.cache/setup.cache"
+    if [ ! -s "$cache" ]; then
+        echo "$ARCH_HASH" > "$cache"
+    else
+        sed -i "1s/.*/$ARCH_HASH/" "$cache"
+    fi
 fi
