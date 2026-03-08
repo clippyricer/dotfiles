9e3edb58c4e26e17604aa925e5be7e9f9965683b
clippyricer <bannansmooth68@proton.me>
2026-03-07 20:19:41 -0500

    improve flake not done yet

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "A Flake providing everything needed";
+  description = "A Flake to download dependencies";
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
@@ -7,23 +7,29 @@
     let
       systems = [ "x86_64-linux" "aarch64-linux" ];
       forAllSystems = f: nixpkgs.lib.genAttrs systems (system:
-        f {
-          pkgs = import nixpkgs { inherit system; };
-          system = system;
-        }
+        f (import nixpkgs { inherit system; })
       );
     in {
-      packages = forAllSystems ({ pkgs, ... }: {
-        default = pkgs.buildEnv {
-          name = "dependencies";
+      packages = forAllSystems (pkgs: {
+        default = pkgs.symlinkJoin {
+          name = "deps";
           paths = [
             pkgs.jq
-            pkgs.dunst
+            pkgs.python315
             pkgs.zip
             pkgs.unzip
             pkgs.tmux
-            pkgs.python315
+            pkgs.fastfetch
           ];
+        };
+      });
+
+      apps = forAllSystems (pkgs: {
+        default = {
+          type = "app";
+          # This points to 'true', which does nothing. 
+          # But to get here, Nix MUST download everything in 'packages.default' first.
+          program = "${pkgs.coreutils}/bin/true";
         };
       });
     };
